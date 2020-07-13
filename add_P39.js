53b4e828f00788b497f397af4a9d640eefce37bf
Tony Bowden <tony@tmtm.com>
2020-07-14 00:20:14 +0100

    Template to add a new sourced P39 statement

---
+++ add_P39.js
@@ -0,0 +1,13 @@
+// add_P39.js
+module.exports = id => ({
+  id,
+  claims: {
+    P39: {
+      value: 'Q2484309',
+      references: {
+        P143: 'Q328',
+        P4656: 'https://en.wikipedia.org/wiki/Home_Secretary'
+      },
+    }
+  }
+})
