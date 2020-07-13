bca33e8a7723e7951794235cbe8dfd748a6d8313
Tony Bowden <tony@tmtm.com>
2020-07-14 00:06:50 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,68 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'csv'
+require 'pry'
+require 'scraped'
+require 'wikidata_ids_decorator'
+
+require_relative 'lib/unspan_all_tables'
+
+class MembersPage < Scraped::HTML
+  decorator WikidataIdsDecorator::Links
+  decorator UnspanAllTables
+
+  field :officeholders do
+    list.xpath('.//tr[td]').map { |td| fragment(td => HolderItem) }.reject(&:empty?).map(&:to_h).uniq(&:to_s)
+  end
+
+  private
+
+  def list
+    noko.xpath('//h2[contains(.,"List of Home Secretaries")]//following::table[1]')
+  end
+end
+
+class HolderItem < Scraped::HTML
+  field :id do
+    tds[2].css('a/@wikidata').map(&:text).first
+  end
+
+  field :name do
+    tds[2].css('a/@title').map(&:text).map(&:tidy).first
+  end
+
+  field :start_date do
+    Date.parse(start_text) rescue nil
+  end
+
+  field :end_date do
+    return if end_text == 'Incumbent'
+    Date.parse(end_text) rescue nil
+  end
+
+  def empty?
+    name.to_s == ''
+  end
+
+  private
+
+  def tds
+    noko.css('td,th')
+  end
+
+  def start_text
+    tds[3].text.tidy
+  end
+
+  def end_text
+    tds[4].text.tidy
+  end
+end
+
+url = URI.encode 'https://en.wikipedia.org/wiki/Home_Secretary'
+data = Scraped::Scraper.new(url => MembersPage).scraper.officeholders
+
+header = data.first.keys.to_csv
+rows = data.map { |row| row.values.to_csv }
+puts header + rows.join
