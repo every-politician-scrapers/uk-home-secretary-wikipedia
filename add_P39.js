7d187752f28c3e5cad7e26384a5b97d696dc802e
Tony Bowden <tony@tmtm.com>
2020-07-14 08:37:37 +0100

    Add some documentation to add_P39.js
    This also makes it more obvious when copying it to a new project that I
    still need to update the ids!

--- add_P39.js
+++ add_P39.js
@@ -3,10 +3,10 @@ module.exports = id => ({
   id,
   claims: {
     P39: {
-      value: 'Q2484309',
+      value: 'Q2484309', // position held: Home Secretary
       references: {
-        P143: 'Q328',
-        P4656: 'https://en.wikipedia.org/wiki/Home_Secretary'
+        P143: 'Q328',    // imported from: English Wikipedia
+        P4656: 'https://en.wikipedia.org/wiki/Home_Secretary' // import URL
       },
     }
   }
